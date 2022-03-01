fe2c6cc8b64c21226c8058e4eef2856b6713249e
jcvnttdata <jcvnttdata@gmail.com>
2022-03-01 00:20:13 -0500

    create project

---
+++ src/test/java/com/nttdata/project_bank/ProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.project_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
